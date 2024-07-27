775a587ac3e090c48d8114c66e16ebbe9c9560a7
MissMurigi <vickymurigi001@gmail.com>
2024-07-27 09:17:07 +0300

    MeL

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//write your code here!
+const main = document.querySelector("main#main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = `Victoria is the champion`;
+
+document.body.appendChild(newHeader);
